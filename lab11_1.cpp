2f2b98ac5c23a4f56ec43e0b5ca8baeb7cc0998e
wh0ohm <wararat.boonchieng@gmail.com>
2024-12-24 13:49:36 +0700

    lab11-1 complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,32 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+
+using namespace std;
+
+int main(){
+    int i = 0;
+    cout << "Press Enter 3 times to reveal your future."  ;
+    while(true){
+        if(i==3) break;
+        if(cin.get() == '\n'){
+            i++;
+        }
+    }
+    srand(time(0));
+    int x = rand()%9;
+    if(x == 0) cout << "You will get A in this 261102.";
+    else if(x == 1) cout << "You will get B+ in this 261102.";
+    else if(x == 2) cout << "You will get B in this 261102.";
+    else if(x == 3) cout << "You will get C+ in this 261102.";
+    else if(x == 4) cout << "You will get C in this 261102.";
+    else if(x == 5) cout << "You will get D+ in this 261102.";
+    else if(x == 6) cout << "You will get D in this 261102.";
+    else if(x == 7) cout << "You will get F in this 261102.";
+    else if(x == 8) cout << "You will get W in this 261102.";
+    
+    return 0;
+}
